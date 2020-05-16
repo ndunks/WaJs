c0450c1918ef1af6b6399bf1444c60cb362fa9e5
Mochamad Arifin <klampok.child@gmail.com>
2020-05-16 22:12:03 +0700

    Manual test no jasmine

---
+++ src/test.ts
@@ -0,0 +1,3 @@
+import "./utils"
+
+import "./test/read-preempt"
--- src/test/read-preempt.ts
+++ src/test/read-preempt.ts
@@ -1,19 +1,11 @@
-/*
-Try and error
-
-node_modules/.bin/tsc-watch --onFirstSuccess \
-'nodemon --delay 0.5 -w dist -e js -x node dist/test/read-preempt.js'
-
- */
-import "./utils"
 import * as fs from "fs"
-import BinaryBuffer from "@/whatsapp/binary/buffer";
-import { readNode } from "@/whatsapp/binary/reader";
-import BufferReader from "@/whatsapp/binary/buffer-reader";
+import BinaryBuffer from "../whatsapp/binary/buffer";
+import { readNode } from "../whatsapp/binary/reader";
+import BufferReader from "../whatsapp/binary/buffer-reader";
 import assert from "assert";
 
 
-const dir = `test/binary-sample`
+const dir = `etc/binary-sample`
 /** First preempt */
 const file = fs.readdirSync(dir).filter(v => v.match(/^preempt/))[0]
 const nodeBuffer = fs.readFileSync(`${dir}/${file}`)
