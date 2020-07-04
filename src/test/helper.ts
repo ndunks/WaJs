c309885d19e9aa952dfc082775a3bea5e247d3be
Mochamad Arifin <klampok.child@gmail.com>
2020-07-04 15:57:57 +0700

    fix wa not loaded

--- src/test/helper.ts
+++ src/test/helper.ts
@@ -5,7 +5,10 @@ let wa_instance: WhatsApp
 
 /** Get WA after chats-loaded */
 export function testHelperLoadWa(): Promise<WhatsApp> {
+
+    // don't reinit if already there
     if (wa_instance) return Promise.resolve(wa_instance)
+
     const wa = new WhatsApp();
     return new Promise((res, rej) => {
         wa.connect().then(
@@ -21,18 +24,22 @@ export function testHelperLoadWa(): Promise<WhatsApp> {
     })
 }
 
-export function testHelperDisconnectWa(afterSecond: number = 1) {
-    return new Promise((res, rej) => {
+export function testHelperDisconnectWa(afterSecond: number = null) {
+    if (!wa_instance) return;
+    if (afterSecond === null) {
         // just disable watchdog
         wa_instance.client.ws.stopWatchdog()
-        /* setTimeout(() => {
+    }
+
+    return new Promise((res, rej) => {
+        setTimeout(() => {
             try {
                 wa_instance.close()
                 res(true)
             } catch (error) {
                 rej(error)
             }
-        }, afterSecond * 1000) */
+        }, afterSecond * 1000)
     })
 }
 
