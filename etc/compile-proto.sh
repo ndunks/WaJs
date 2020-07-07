e0b8cdd57a6a85b239bbeba8515013d60266d146
Mochamad Arifin <klampok.child@gmail.com>
2020-07-07 13:26:44 +0700

    remove protoc-ts as dev

--- etc/compile-proto.sh
+++ etc/compile-proto.sh
@@ -2,10 +2,13 @@
 # Generate protobuf js/ts from proto2 declaration
 # Run from root directory
 
-PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
+# Get proto compiler & Install TS Generator
+#   yarn global add ts-protoc-gen
+#   protoc: https://github.com/protocolbuffers/protobuf/releases
+
+PROTOC_GEN_TS_PATH=$(which protoc-gen-ts)
 OUT_DIR="./src"
 PROTO_FILE="whatsapp.proto"
-# Get protoc: https://github.com/protocolbuffers/protobuf/releases
 protoc \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --js_out="import_style=commonjs,binary:${OUT_DIR}" \
