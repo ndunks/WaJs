a45dd3f13d906d672df96b9acd4f923ebb54fd1e
Mochamad Arifin <klampok.child@gmail.com>
2020-07-07 12:09:08 +0700

    export required

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import WhatsApp from "./whatsapp"
+import store from "./store"
+import { widHelper } from "./whatsapp/helper"
+import { Color } from "./utils"
+
+export { WhatsApp, store, widHelper, Color }
