4c324a6cc937116e0d04077babab19db6a6c9ce1
Mochamad Arifin <klampok.child@gmail.com>
2020-07-07 12:09:59 +0700

    filter

--- test/test.ts
+++ test/test.ts
@@ -1,39 +1,30 @@
-import moduleAlias from "module-alias"
+import path from "path";
+import fs from "fs";
 
-const project_dir = process.cwd();
+import { build_dir } from "./helper/path-helper";
+import { testHelperDisconnectWa } from "./helper/wa-helper"
 
-moduleAlias.addAliases({
-    "@": `${project_dir}/build-test/src`,
-    "whatsapp_pb": `${project_dir}/etc/whatsapp_pb.js`,
-    "../whatsapp_pb": `${project_dir}/etc/whatsapp_pb.js`
-})
-
-import { testHelperSequential, testHelperDisconnectWa } from "./test/helper"
-
+/** Pass filter to a single test file by basename. */
 let filter = process.argv.find((v, i, a) => i > 0 && a[i - 1] == '--filter');
-// const tests = {
-//     "protobuf": {  },
-//     "readbin-preempt": {  },
-//     "readbin-message": {  },
-//     "readbin-writenode": {  },
-// };
+let tests = fs.readdirSync(path.join(build_dir, 'test/test'), 'ascii')
+    .map(
+        f => path.basename(f, '.js')
+    ).filter(
+        f => !filter || f == filter
+    )
 
-console.log('Filter', filter);
-import "./test/protobuf"
-import "./test/readbin-preempt"
-import "./test/readbin-message"
-import "./test/readbin-writenode"
-// testHelperSequential([
-//     //require("./test/whatsapp-sendbin").default,
-//     //require("./test/whatsapp-presence").default,
-//     //require("./test/whatsapp-chats").default,
-//     require("./test/whatsapp-sendmessage").default,
-//     //require("./test/whatsapp-autoreply").default,
-// ]).then(
-//     (stats) => {
-//         if (stats.fail) {
-//             console.log('WhatsApp Test', stats)
-//         }
-//         setTimeout(() => testHelperDisconnectWa(), 2000)
-//     }
-// )
+if (!tests.length) {
+    console.log("No test to run");
+    process.exit(1);
+}
+
+(async function () {
+    for (let f of tests) {
+        console.log(`--- ${f} ---`)
+        let ret = require(`./test/${f}`).default();
+        if (ret instanceof Promise) {
+            await ret;
+        }
+    }
+    return testHelperDisconnectWa();
+})()
